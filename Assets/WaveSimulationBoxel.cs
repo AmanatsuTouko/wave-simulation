4ee1ca62763aea2dfbae320ef0888b996e73bdf9
AmanatsuToko <88881947+AmanatsuTouko@users.noreply.github.com>
2025-07-01 20:34:08 +0900

    不要な関数を削除，Voxelのスペルミスを修正

--- Assets/WaveSimulationBoxel.cs
+++ Assets/WaveSimulationBoxel.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 public class WaveSimulationBoxel : MonoBehaviour
 {
     public int gridSize = 100;
-    // public float spacing = 0.1f;
     public float spacing = 1.0f;
     public float waveSpeed = 1.0f;
     public float timeStep = 0.02f;
@@ -14,14 +13,12 @@ public class WaveSimulationBoxel : MonoBehaviour
 
     public float impulse = 1.0f;
 
-    private Mesh mesh;
-    private Vector3[] vertices;
     private float[,] prev;
     private float[,] current;
     private float[,] next;
 
     public GameObject boxelPrefab;
-    public GameObject[,] boxels;
+    public GameObject[,] voxels;
 
     public GameObject spherePrefab;
     private GameObject sphere;
@@ -29,10 +26,9 @@ public class WaveSimulationBoxel : MonoBehaviour
 
     void Start()
     {
-        boxels = new GameObject[gridSize, gridSize];
+        voxels = new GameObject[gridSize, gridSize];
 
-        // GenerateMesh();
-        GenerateBoxel();
+        GenerateVoxel();
 
         GameObject sphereObject = GameObject.Instantiate(spherePrefab);
         sphereObject.transform.position = new Vector3(gridSize / 2, sphereInitHeight, gridSize / 2);
@@ -47,7 +43,7 @@ public class WaveSimulationBoxel : MonoBehaviour
     void Update()
     {
         SimulateWave();
-        ApplyToMesh();
+        ApplyToVoxels();
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -95,25 +91,17 @@ public class WaveSimulationBoxel : MonoBehaviour
         next = temp;
     }
 
-    void ApplyToMesh()
+    void ApplyToVoxels()
     {
         for (int i = 0; i < gridSize; i++)
         {
             for (int j = 0; j < gridSize; j++)
             {
-                // int index = i * gridSize + j;
-                // Vector3 v = vertices[index];
-                // v.y = current[i, j];
-                // vertices[index] = v;
-
-                Vector3 pos = boxels[i, j].transform.position;
+                Vector3 pos = voxels[i, j].transform.position;
                 pos.y = current[i, j];
-                boxels[i, j].transform.position = pos;
+                voxels[i, j].transform.position = pos;
             }
         }
-
-        // mesh.vertices = vertices;
-        // mesh.RecalculateNormals();
     }
 
     void AddImpulse(int x, int y, float strength)
@@ -124,57 +112,19 @@ public class WaveSimulationBoxel : MonoBehaviour
         }
     }
     
-    void GenerateBoxel()
+    void GenerateVoxel()
     {
         for (int i = 0; i < gridSize; i++)
         {
             for (int j = 0; j < gridSize; j++)
             {
                 // 生成
-                GameObject boxel = GameObject.Instantiate(boxelPrefab);
-                boxels[i, j] = boxel;
+                GameObject voxel = GameObject.Instantiate(boxelPrefab);
+                voxels[i, j] = voxel;
                 
                 // 位置の指定
-                boxel.transform.position = new Vector3(i* spacing, 0, j * spacing);
+                voxel.transform.position = new Vector3(i* spacing, 0, j * spacing);
             }
         }
-    }
-
-    void GenerateMesh()
-    {
-        mesh = new Mesh();
-        GetComponent<MeshFilter>().mesh = mesh;
-
-        vertices = new Vector3[gridSize * gridSize];
-        int[] triangles = new int[(gridSize - 1) * (gridSize - 1) * 6];
-
-        for (int i = 0; i < gridSize; i++)
-        {
-            for (int j = 0; j < gridSize; j++)
-            {
-                vertices[i * gridSize + j] = new Vector3(i * spacing, 0, j * spacing);
-            }
-        }
-
-        int t = 0;
-        for (int i = 0; i < gridSize - 1; i++)
-        {
-            for (int j = 0; j < gridSize - 1; j++)
-            {
-                int index = i * gridSize + j;
-
-                triangles[t++] = index;
-                triangles[t++] = index + gridSize;
-                triangles[t++] = index + gridSize + 1;
-
-                triangles[t++] = index;
-                triangles[t++] = index + gridSize + 1;
-                triangles[t++] = index + 1;
-            }
-        }
-
-        mesh.vertices = vertices;
-        mesh.triangles = triangles;
-        mesh.RecalculateNormals();
     }
 }
